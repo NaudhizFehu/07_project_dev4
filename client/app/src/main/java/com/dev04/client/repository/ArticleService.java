376877a5c5d3c9b3bffb66f08afaff8e358cd6dc
signatureh <signature-h@hanmail.net>
2021-05-18 01:01:59 +0900

    게시판 목록 뷰단 구현 ArticleRepository.java / ArticleService.java / ArticleVO.java 작성 ListAdapter.java / ListFragment.java / ListViewModel.java / fragment_list.xml / item_list.xml / item_list_header.xml 작성

---
+++ client/app/src/main/java/com/dev04/client/repository/ArticleService.java
@@ -0,0 +1,35 @@
+package com.dev04.client.repository;
+
+
+import com.dev04.client.viewObject.LoginInput;
+import com.dev04.client.viewObject.MemberVO;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ArticleService {
+
+//    @GET("member")
+//    Call<List<Member>> getAllMembers();
+//
+//    @POST("member/login")
+//    Call<MemberVO> loginMember(@Body LoginInput loginInput);
+//
+//    @Multipart
+//    @POST("member/sign-up")
+//    Call<Void> signUpMember(@Part("member") Member member, @Part MultipartBody.Part profileFile);
+//
+//    @Multipart
+//    @POST("member/modify-profile")
+//    Call<Void> modifyMemberWithProfile(@Part("member") Member member, @Part MultipartBody.Part profileFile);
+//
+//    @PUT("member/modify")
+//    Call<Void> modifyMember(@Body Member member);
+//
+//    @DELETE("member")
+//    Call<Void> deleteMembers(@Query("targets") String targets);
+//
+//    @GET("member/{name}")
+//    Call<List<Member>> getMembersByName(@Path("name") String name);
+}
---
+++ client/app/src/main/java/com/dev04/client/viewObject/ArticleVO.java
@@ -0,0 +1,74 @@
+package com.dev04.client.viewObject;
+
+public class ArticleVO {
+    private int no;
+    private String title;
+    private String writerId;
+    private String writerName;
+    private String content;
+    private String regDate;
+    private int readCnt;
+
+    public ArticleVO(String title, String writerId, String writerName, String content) {
+        this.title = title;
+        this.writerId = writerId;
+        this.writerName = writerName;
+        this.content = content;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getWriterId() {
+        return writerId;
+    }
+
+    public String getWriterName() {
+        return writerName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public int getReadCnt() {
+        return readCnt;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setWriterId(String writerId) {
+        this.writerId = writerId;
+    }
+
+    public void setWriterName(String writerName) {
+        this.writerName = writerName;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+
+    public void setReadCnt(int readCnt) {
+        this.readCnt = readCnt;
+    }
+}
