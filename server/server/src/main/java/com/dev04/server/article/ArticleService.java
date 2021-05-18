b231f1fd88ccd0b36a140b04f89f6a51fce63f99
sungminju7488 <ssmj94@hanmail.net>
2021-05-18 14:47:42 +0900

    ArticleVO.java -->@SequenceGenerator 변경

--- server/server/src/main/java/com/dev04/server/article/ArticleService.java
+++ server/server/src/main/java/com/dev04/server/article/ArticleService.java
@@ -13,19 +13,23 @@ import org.springframework.stereotype.Service;
 public class ArticleService {
 	@Autowired
 	 private Repository rp;
-	 
+	 //글쓰기
 	public void writeArticle(ArticleVO vo) {
 		rp.save(vo);
 	}
+	//글수정
 	public void updateArticle(ArticleVO vo) {
 		rp.save(vo);
 	}
-	public Optional<ArticleVO> readArticle(ArticleVO vo) {
-		 return rp.findById(String.format("%d", vo.getNo()));
-	}
+	//글삭제
 	public void deleteArticle(ArticleVO vo) {
 		rp.delete(vo);
 	}
+	//게시글읽기
+	public Optional<ArticleVO> readArticle(ArticleVO vo) {
+		return rp.findByNo(vo.getNo());
+	}
+	
 //	public Page<ArticleVO> articleList(Pageable pageable) {
 //		Page<ArticleVO> articlePage =rp.findAll(pageable);
 //		return articlePage;
--- server/server/src/main/java/com/dev04/server/article/ArticleVO.java
+++ server/server/src/main/java/com/dev04/server/article/ArticleVO.java
@@ -2,6 +2,8 @@ package com.dev04.server.springbootproject.article;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.SequenceGenerator;
@@ -12,11 +14,12 @@ import lombok.Setter;
 
 @Getter@Setter
 @Entity
-@SequenceGenerator(name = "articleseq", sequenceName ="no",initialValue = 1,allocationSize = 1)
+@SequenceGenerator(name = "ARTICLE_SEQ", sequenceName ="NO_SEQ",initialValue = 1,allocationSize = 1)
 @Table(name="ARTICLE")
 public class ArticleVO {
 	@Id
 	@Column(name="NO",nullable = false)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ARTICLE_SEQ")
 	private int no;
 	
 	@Column(name="TITLE",length=100,nullable = false)
